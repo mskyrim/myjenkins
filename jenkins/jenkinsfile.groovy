4da503b8bf0939b697d7dbc5d81b8465ba2b384e
Mehdi Dardani <mehdi.dardani@accuity.com>
2021-05-04 17:09:02 +0200

    BUTTERFLY-3: my jenkins sources

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -5,14 +5,14 @@ pipeline {
         stage('Checkout') {
             steps {
                 // Get some code from a GitHub repository
-                git branch: 'feature/oren/CONTINUITY-34030-spinteg-transaction', credentialsId: 'bitbucket-ssh-agent-access-key', url: 'ssh://git@dev-bitbucket.fircosoft.net/cty/continuity.git'
+                git branch: '', credentialsId: '', url: ''
             }
 
         }
         stage('Build') {
             steps {
                 // Run Maven on a Unix agent.
-                sh "cd orchestrator && ./gradlew distXTestsOREN"
+                sh ""
 
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
@@ -22,7 +22,7 @@ pipeline {
                 // If Maven was able to run the tests, even if some of the test
                 // failed, record the test results and archive the jar file.
                 success {
-                    archiveArtifacts 'orchestrator/apps/orchestrator/build/dist/*.zip'
+                    archiveArtifacts ''
                 }
             }
         }
